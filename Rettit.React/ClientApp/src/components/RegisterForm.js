c2ed30b280b780471f7843710811933a9f45d4dd
Matthijs van Spelde <I404906@fhict.nl>
2020-03-11 15:49:05 +0100

    Rename

--- Rettit.React/ClientApp/src/components/RegisterForm.js
+++ Rettit.React/ClientApp/src/components/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export class Home extends Component {
+export class RegisterForm extends Component {
 	constructor(props) {
 		super(props)
 
