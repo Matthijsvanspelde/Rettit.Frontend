62c4b64d8eafe293139824f8ce6f32617eef1d74
Matthijs van Spelde <I404906@fhict.nl>
2020-05-11 10:49:36 +0200

    Add get searchresults

--- Rettit.React/ClientApp/src/components/Search.js
+++ Rettit.React/ClientApp/src/components/Search.js
@@ -1,16 +1,55 @@
 ﻿import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: [],
+            query: '',
+        };
+    }
+
+	handleChange = (event) => {
+		event.preventDefault();
+		const { name, value } = event.target;
+		this.setState({ [name]: value });
+	}
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        axios.get('https://rettitapi.azurewebsites.net/api/search/' + this.state.query)
+            .then(res => {
+                this.setState({ results: res.data })
+            }).catch(err => {
+                console.log(err)
+            });
+	}
 
     render() {
         return (
             <div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Search Rettit" aria-describedby="button-addon2" />
-                    <div class="input-group-append">
-                    <button class="btn btn-primary" type="button" id="button-addon2">Search</button>
+                <form onSubmit={this.handleSubmit} noValidate>
+                    <div className="input-group mb-3">
+                        <input className="form-control" type='text' name='query' autoComplete='off' onChange={this.handleChange} noValidate />
+                        <div className='submit input-group-append'>
+                            <button className="btn btn-primary">Search</button>
+                        </div>
                     </div>
-                </div>
+                </form>
+
+                {
+                    this.state.results.map(function (searchResult, i) {
+                        return <i key={i}>
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <a href={'/r/' + searchResult.name} className="card-title text-dark" style={{ fontSize: 24 }}>r/{searchResult.name}</a>
+                                    <p className="card-text">{searchResult.about}</p>
+                                </div>
+                            </div><br />
+                        </i>
+                    })
+                }
             </div>
         );
     }
